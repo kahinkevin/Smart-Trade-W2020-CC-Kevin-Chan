aa58280703b174a88e8ab52c8b018534390cb277
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2019-08-19 14:30:45 -0400

    move script in fetch.py

--- fetch.py
+++ fetch.py
@@ -25,70 +25,48 @@ example:
 
 
 """
-
-import requests
-import lxml.html as lh
-import pandas as pd
-import urllib
+#https://towardsdatascience.com/data-science-skills-web-scraping-javascript-using-python-97a29738353f
+# import libraries
+import urllib.request
 from bs4 import BeautifulSoup
-from tabulate import tabulate
+from selenium import webdriver
+import time
+import pandas as pd
+# specify the url
+urlpage = 'https://kabu.com/investment/meigara/tani_henkou.html' 
+print(urlpage)
+# run firefox webdriver from executable path of your choice
+driver = webdriver.Firefox()
 
-def fetch_html_table():
-    print(heh)
 
+# get web page
+driver.get(urlpage)
+# execute script to scroll down the page
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+# sleep for 30s
+print("see you in 5s")
+time.sleep(5)
+print("5s passed")
 
-"""
-reference used : https://towardsdatascience.com/web-scraping-html-tables-with-python-c9baba21059
-"""
-if __name__ == '__main__':
-    # url = 'https://kabu.com/investment/meigara/tani_henkou.html'
-    # html = requests.get(url).content
-    # df_list = pd.read_html(html)
-    # df = df_list[-1]
-    # print(df)
-    # df.to_csv('my data.csv')
-    
-    
-    
-    #pd.read_html(requests.get(https://kabu.com/investment/meigara/tani_henkou.html).content)[-1].to_csv("fetch.csv")
-    
-    # https://pythonprogramminglanguage.com/web-scraping-with-pandas-and-beautifulsoup/
-    res = requests.get("https://kabu.com/investment/meigara/tani_henkou.html")
-    soup = BeautifulSoup(res.content,'lxml')
-    table = soup.find_all("table")
-    print(table)
-    # df = pd.read_html(str(table))
-    # print( tabulate(df[0], headers='keys', tablefmt='psql') )
-    
-    
-    # reference : https://stackoverflow.com/questions/10556048/how-to-extract-tables-from-websites-in-python/44506462
-    # web = urllib.urlopen("https://kabu.com/investment/meigara/tani_henkou.html")
-    # s = web.read()
-    
-    # html = etree.HTML(s)
-    
-    # ## Get all 'tr'
-    # tr_nodes = html.xpath('//table[class="tbl01"]/tr')
-    
-    # ## 'th' is inside first 'tr'
-    # header = [i[0].text for i in tr_nodes[0].xpath("th")]
-    
-    # ## Get text from rest all 'tr'
-    # td_content = [[td.text for td in tr.xpath('td')] for tr in tr_nodes[1:]]
-    
-    
-    # url = 'https://kabu.com/investment/meigara/tani_henkou.html'
+# find elements by xpath
+results = driver.find_elements_by_xpath("//*[@id='main']/table")
+print('Number of results', len(results))
 
-    # #Create a handle, page, to handle the contents of the website
-    # page = requests.get(url)
-    # #Store the contents of the website under doc
-    # doc = lh.fromstring(page.content)
-    # print(doc)
-    # #Parse data that are stored between <tr>..</tr> of HTML
-    # tr_elements = doc.xpath('//tr')
-    
-    # #Check the length of the first 12 rows
-    # print(len(tr_elements))
-    # for T in tr_elements[:12]:
-    #     print(len(T))
-        
+#https://stackoverflow.com/a/49996714/9876427
+df_list = pd.read_html(results[0].get_attribute('outerHTML'))
+df = df_list[-1]
+print(df)
+df.to_csv('my_data.csv')
+
+# create empty array to store data
+data = []
+# loop over results
+# for result in results:
+#     product_name = result.text
+#     link = result.find_element_by_tag_name('a')
+#     product_link = link.get_attribute("href")
+#     # append dict to array
+#     data.append({"product" : product_name, "link" : product_link})
+
+# close driver 
+driver.quit()        
--- untitled0.py
+++ untitled0.py
@@ -15,49 +15,7 @@ Created on Sun Aug 18 23:42:52 2019
 # le plus prometteur de 11h30  à 12h01
 #https://towardsdatascience.com/data-science-skills-web-scraping-javascript-using-python-97a29738353f
 
-# import libraries
-import urllib.request
-from bs4 import BeautifulSoup
-from selenium import webdriver
-import time
-import pandas as pd
-# specify the url
-urlpage = 'https://kabu.com/investment/meigara/tani_henkou.html' 
-print(urlpage)
-# run firefox webdriver from executable path of your choice
-driver = webdriver.Firefox()
 
-
-# get web page
-driver.get(urlpage)
-# execute script to scroll down the page
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-# sleep for 30s
-print("see you in 5s")
-time.sleep(5)
-print("5s passed")
-
-# find elements by xpath
-results = driver.find_elements_by_xpath("//*[@id='main']/table")
-print('Number of results', len(results))
-
-df_list = pd.read_html(results[0].get_attribute('outerHTML'))
-df = df_list[-1]
-print(df)
-df.to_csv('my_data.csv')
-
-# create empty array to store data
-data = []
-# loop over results
-# for result in results:
-#     product_name = result.text
-#     link = result.find_element_by_tag_name('a')
-#     product_link = link.get_attribute("href")
-#     # append dict to array
-#     data.append({"product" : product_name, "link" : product_link})
-
-# close driver 
-driver.quit()
 
 
 
