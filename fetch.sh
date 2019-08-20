2b34eac1b666f58d40dc10d71421ced1cfd8ce69
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2019-08-20 13:14:06 -0400

    shorter shell script
    virtual env probably not needed

--- fetch.sh
+++ fetch.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# https://stackoverflow.com/a/42907421/9876427 for installing pip et virtualenv
-# https://stackoverflow.com/a/25735801/9876427
-
-#python -m venv .
-virtualenv -q -p . $1
-source $1/bin/activate
+python -m venv .
 pip install -r requirements.txt
 
 python ./fetch.py
